c21af619c9fe765c2a82fe2124ba37556a037e24
Marc-Antoine <marco.nissa.06@gmail.com>
2019-09-22 12:25:31 +0200

    test?

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Boulé & Corp of company of the dead
         </a>
       </header>
     </div>
